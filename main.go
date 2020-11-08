d734d8df1e9e65d871dbaa58cd1c5bdedd0f58cc
yishai <yishai5555@gmail.com>
2020-11-08 15:12:13 +0200

    04-interfce

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -13,24 +17,14 @@ func main() {
 	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
-}
-
-func (eb englishBot) getGreeting() string {
+func (englishBot) getGreeting() string {
 	return "Hi There !"
 }
 
-func (sb spanishBot) getGreeting() string {
-	return "Hi There !"
+func (spanishBot) getGreeting() string {
+	return "Hola !"
 }
-
-// func printMap(c map[string]string) {
-// 	for color, hex := range c {
-// 		fmt.Println("Hex code for", color, " is ", hex)
-// 	}
-// }
